308ff05b29ba6cffb7b1d860dbdbe388f36f26ac
Ricky Yaramis <ricky.yaramis@student.odisee.be>
2024-12-11 15:16:18 +0000

    login

---
+++ login/gitclone.sh
@@ -0,0 +1,8 @@
+cd ..
+cd ..
+mkdir temp
+cd temp
+git clone https://github.com/Rickyyar/login.git
+cd login
+chmod +x setuplogin.sh
+bash setuplogin.sh
---
+++ login/setuplogin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cleanup() {
+    echo "Cleaning up ..."
+    deactivate
+    cd /home/devasc/Downloads/
+    rm -rf /home/devasc/Downloads/temp/
+    echo "Application folder has been removed"
+}
+trap cleanup EXIT
+echo "$(date '+%H:%M:%S') - creating python VENV"
+python3 -m venv login_venv
+sleep 15  # Pause
+echo "$(date '+%H:%M:%S') - This is the message after a 15-second pause."²
+echo "$(date '+%H:%M:%S')- entering VENV"
+source login_venv/bin/activate
+pip install -r requirements.txt
+echo "$(date '+%H:%M:%S') - starting python application"
+python3 flask_app.py
+echo "$(date '+%H:%M:%S') - application stopped"
